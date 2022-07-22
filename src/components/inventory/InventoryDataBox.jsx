95ecbb6720eccefb8d701cc1f6efd7e7a31db393
Rudhamoy <rudhamoydebbarma@gmail.com>
2022-07-22 23:24:45 +0530

    InventoryDatabox css edited

--- src/components/inventory/InventoryDataBox.jsx
+++ src/components/inventory/InventoryDataBox.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const InventoryDataBox = ({icon, title, numData}) => {
   return (
-    <div className="bg-white border shadow-md p-3 rounded-md w-[180px] h-[120px] relative">
+    <div className="bg-white border shadow-md p-3 rounded-md w-[150px] h-[120px] relative">
         {/* icon */}
         <img src={icon} alt="icoon" className="w-[20px]" />
         {/* title */}
-        <p className="text-gray-400 text-sm font-semibold my-2 mt-3">{title}</p>
+        <p className="text-gray-400 text-xs font-semibold my-2 mt-3">{title}</p>
         {/* number data */}
         <p className="font-bold text-xl absolute bottom-3">{numData}</p>
     </div>
